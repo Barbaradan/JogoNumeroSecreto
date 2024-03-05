bd6468d0e6d3444e6d7583e3db3dade16c224aa2
Barbara Daniel <105955206+Barbaradan@users.noreply.github.com>
2024-03-05 11:05:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-//anotações e comentarios em Notion > progs
+let listaDeNumerosSorteados = []; //guada os nº já sorteados
+let numeroLimite = 10; 
+let numeroSecreto = gerarNumAleatorio();
+let tentativas = 1; 
 
-//let titulo = document.querySelector('h1'); //h1 do html 
-//titulo.innerHTML = 'Jogo do Número Secreto'; 
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
-let listaDeNumerosSorteados = []; //array vazio p guadar os nº já sorteados
-let numeroLimite = 10; //linha 51
-let numeroSecreto = gerarNumAleatorio(); //var q recebe a function
-let tentativas = 1; //nº de tentativas
-
-function exibirTexto(tag, texto) { //evitar repetição do let titulo e paragrafo
-    let campo = document.querySelector(tag); //tag pq oq muda nas linhas é só a tag h1 e p, pq a sintaxe é a mesma
-    campo.innerHTML = texto; //texto pq o que muda é o txt
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2}); //referente linha 7 html
+function exibirTexto(tag, texto) { 
+    let campo = document.querySelector(tag); 
+    campo.innerHTML = texto; //oq muda é o txt
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2}); //referente script html 
 }
 
-function exibirMsgInicial() { //sempre que reinicar o jogo
-    exibirTexto('h1', 'Jogo do Número Secreto'); //executa a function - parametros (tag, texto)
+function exibirMsgInicial() { 
+    exibirTexto('h1', 'Jogo do Número Secreto'); //executa a function, parametros (tag, texto)
     exibirTexto('p', 'Escolha um número de 1 a 10');
 }
 
     exibirMsgInicial(); 
 
-function verificarChute() { //determina o que o evento onclick faz
-    let chute = document.querySelector('input').value; //o valor do input da linha 25 html
+function verificarChute() { //onclick
+    let chute = document.querySelector('input').value;
     
     if(chute == numeroSecreto){
         exibirTexto('h1', 'Acertou!'); //function (tag, texto)
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; //é > q 1? se sim : se não
         let msgTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
         exibirTexto('p', msgTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled'); //id do html do btn Novo Jogo - linha 28, remove o disabled "desativa" ele
+        document.getElementById('reiniciar').removeAttribute('disabled'); //id btn Novo Jogo desativa disabled
 
     } else {
         if (chute > numeroSecreto){
@@ -40,8 +32,8 @@ function verificarChute() { //determina o que o evento onclick faz
         } else {
             exibirTexto('p', 'O número secreto é maior');
         }
-            tentativas++; //soma o nº de tentativas
-            limparCampo(); //limpar o input 
+            tentativas++; 
+            limparCampo(); //limpa input 
     }
 }
 
@@ -56,15 +48,15 @@ function gerarNumAleatorio() {
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) { //verifica se na lista já tem o nº escolhido
         return gerarNumAleatorio(); //vai retornar o gerar n° (p gerar outro nº caso já tiver incluso o nº escolhido)
     } else {
-        listaDeNumerosSorteados.push(numeroEscolhido); //o push add itme ao final da lista. vai colocar o nº q foi escolhido, já q ele ñ tem incluso na lista
+        listaDeNumerosSorteados.push(numeroEscolhido); 
         console.log(listaDeNumerosSorteados);
         return numeroEscolhido;
     }
 }
 
-function limparCampo() { //pra limpar o input após uma tentativa
-    chute = document.querySelector('input'); //pegamos o chute que ta no input
-    chute.value = ''; //queremos que o valor do chute seja vazio (limpo)
+function limparCampo() { 
+    chute = document.querySelector('input'); 
+    chute.value = ''; 
 }
 
 function reiniciarJogo() {
